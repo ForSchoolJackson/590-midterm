c003c31526e50a2f703b2acadead7a4b8f226ccb
ForSchoolJackson <jmh4687@g.rit.edu>
2025-03-14 00:36:08 -0400

    fix maybe

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('public'));
